235fa7de5bfc4a94c4a3231e681343e71d2ad8ea
semyers189 <semyers@fullsail.edu>
2023-09-14 09:45:29 -0400

    added deck rules and filtering rules

--- util/RandomDeckGenerator.js
+++ util/RandomDeckGenerator.js
@@ -4,7 +4,18 @@ const RandomDeckGenerator = async () => {
 
   const cards = await GetCards();
 
-  console.log(cards.length);
+  console.log(cards);
+
+  //STANDARD RULES
+  // 1. 40 CARDS deck.length == 40
+  // 2. ONLY STANDARD CARDS card.formats contains standard
+  // 3. only 6 champion cards supertype = Champion, type == unit
+  // 4. only two regions
+
+  // filtering rules
+  // 1. filter out rarity = none (gets rid of tokens)
+  // 2. get a random card
+  // 2. if it is a champion add it to the champion count
 
   return deck;
 };
