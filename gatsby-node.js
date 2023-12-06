ea4ad36ca92cf6798dda8e4d38becdce047f7e70
semyers189 <semyers@fullsail.edu>
2023-12-05 22:52:10 -0500

    added card gen to ghraphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,15 +4,48 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
+const { GetCards } = require('./util/GetCards');
+
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    path: '/using-dsg',
+    component: require.resolve('./src/templates/using-dsg.js'),
     context: {},
     defer: true,
-  })
-}
+  });
+};
+
+/**
+ * @type {import('gatsby').GatsbyNode['sourceNodes']}
+ */
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode } = actions;
+
+  let allCards = await GetCards();
+  const myData = { cards: allCards, key: 123 };
+
+  const nodeContent = JSON.stringify(myData);
+
+  const nodeMeta = {
+    id: createNodeId(`my-data-${myData.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `myCards`,
+      mediaType: `text/html`,
+      content: nodeContent,
+      contentDigest: createContentDigest(myData),
+    },
+  };
+
+  const node = Object.assign({}, myData, nodeMeta);
+  createNode(node);
+};
